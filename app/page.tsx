cfac8afb889a60d430e30d0f493d6b47365aa128
Martin Fort <martinfort13@gmail.com>
2026-02-25 15:11:20 -0300

    Fix component filename casing for Vercel

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Experience from "@/components/Experience";
 import Intro from "@/components/Intro"
 import Projects from "@/components/Projects";
 import SectionDivider from "@/components/SectionDivider";
-import Skills from "@/components/skills";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
